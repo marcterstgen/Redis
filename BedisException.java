424c4b7f304efe73f23c4e4b220f77a4254a647f
yanglong <874037310@qq.com>
2019-08-05 18:40:19 +0800

    1

---
+++ BedisException.java
@@ -0,0 +1,22 @@
+package y.c.a;
+
+public class BedisException extends Exception {
+    public BedisException() {
+    }
+
+    public BedisException(String message) {
+        super(message);
+    }
+
+    public BedisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BedisException(Throwable cause) {
+        super(cause);
+    }
+
+    public BedisException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
